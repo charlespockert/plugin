d33b955fdb747683603b02877f19da59389afb15
Nik <nik@congral.com>
2015-11-22 22:20:51 -0500

    updated src folder

--- dist/system/index.js
+++ dist/system/index.js
@@ -1,14 +1,80 @@
-System.register([], function (_export) {
-  'use strict';
+System.register(['kendo-ui/styles/web/kendo.common.min.css'], function (_export) {
+	'use strict';
 
-  _export('configure', configure);
+	var kendoConfigBuilder;
 
-  function configure(config) {
-    config.globalResources('./hello-world');
-  }
+	var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-  return {
-    setters: [],
-    execute: function () {}
-  };
+	_export('configure', configure);
+
+	function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } }
+
+	function configure(aurelia, configCallback) {
+
+		var builder = new kendoConfigBuilder();
+
+		if (configCallback !== undefined && typeof configCallback === 'function') {
+			configCallback(builder);
+		}
+
+		if (builder.resources.length === 0) {
+			console.warn("Nothing specified for kendo configuration - using defaults for Kendo Core");
+			builder.core();
+		}
+
+		var resources = builder.resources;
+
+		resources = resources.map(function (r) {
+			return r + "/" + r;
+		});
+
+		aurelia.globalResources(resources);
+	}
+
+	return {
+		setters: [function (_kendoUiStylesWebKendoCommonMinCss) {}],
+		execute: function () {
+			kendoConfigBuilder = (function () {
+				function kendoConfigBuilder() {
+					_classCallCheck(this, kendoConfigBuilder);
+
+					this.resources = [];
+				}
+
+				_createClass(kendoConfigBuilder, [{
+					key: 'core',
+					value: function core() {
+						this.kendoButton().kendoTabStrip();
+						return this;
+					}
+				}, {
+					key: 'pro',
+					value: function pro() {
+						this.core().kendoAutoComplete();
+						return this;
+					}
+				}, {
+					key: 'kendoButton',
+					value: function kendoButton() {
+						this.resources.push("button");
+						return this;
+					}
+				}, {
+					key: 'kendoTabStrip',
+					value: function kendoTabStrip() {
+						this.resources.push("tabstrip");
+						return this;
+					}
+				}, {
+					key: 'kendoAutoComplete',
+					value: function kendoAutoComplete() {
+						this.resources.push("autocomplete");
+						return this;
+					}
+				}]);
+
+				return kendoConfigBuilder;
+			})();
+		}
+	};
 });
